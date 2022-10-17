07c3ec9651955146d3123a462f8407d9d430573e
bguedes <bruno.guedes@mozantech.com>
2022-10-17 12:01:03 +0100

    wip: ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,22 @@
 import type { AppProps } from "next/app";
 import "styles/custom-animate.css";
 import { GlobalStyles } from "global-styles";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script id="GoogleAnalytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+        `}
+      </Script>
       <GlobalStyles />
       <Component {...pageProps} />
     </>
