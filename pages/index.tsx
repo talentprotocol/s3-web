013497a8382c9dd065945b1716b3131bf30e624e
bguedes <bguedesdev@gmail.com>
2022-10-17 11:03:11 +0100

    wip: added meta content

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,13 +31,28 @@ const Home: NextPage<Props> = (props) => {
   return (
     <>
       <Head>
-        <title>Talent Protocol</title>
+        <title>Talent Protocol - Web3 Professional Community</title>
         <meta name="description" content="TALENT APP" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1"
         />
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          property="og:url"
+          content="https://www.talentprotocol.com"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Talent Protocol - Web3 Professional Community"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="og:description"
+          content="Talent Protocol is the web3 professional community where high-potential individuals can build an on-chain resume and launch a talent token, allowing anyone to easily invest in their careers."
+        />
+        <meta property="og:image" content="/metaimage.jpg" />
       </Head>
       <main>
         {isSidebarVisible && (
