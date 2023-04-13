b7980631f74ac8c7f7876377dfc19b732c6a917f
francisco-leal <francisco@talentprotocol.com>
2023-04-13 11:27:33 +0100

    Remove unstoppable domains logo

--- components/partners-banner/index.tsx
+++ components/partners-banner/index.tsx
@@ -10,7 +10,6 @@ import kernelImage from "./assets/kernel.svg";
 import launchHouse from "./assets/launch-house.svg";
 import polygonImage from "./assets/polygon-studios.png";
 import protocolLabsImage from "./assets/protocol-labs.svg";
-import unstoppableDomains from "./assets/unstoppable-domains.png";
 import uTrust from "./assets/utrust.png";
 import { Typography } from "shared-ui";
 import {
@@ -54,10 +53,6 @@ const SLIDER_ENTRIES = [
   },
   {
     image: protocolLabsImage,
-    width: "155px",
-  },
-  {
-    image: unstoppableDomains,
     width: "155px",
   },
   {
